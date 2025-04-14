e83391360e2bbe67cb112b3f18401d88bc480df9
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-14 20:22:12 -0300

    chore: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
+import { Typography } from '@mui/material';
 import { Box, ThemeProvider } from '@mui/system';
+import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Layout } from './components/Layout';
+import { appTheme } from './config/theme';
 
-// const theme = createTheme({});
+const Home = () => (
+  <Box>
+    <Typography variant='h3' component='h1'>
+      Home
+    </Typography>
+  </Box>
+);
+
+const About = () => (
+  <Box>
+    <Typography variant='h3' component='h1'>
+      About
+    </Typography>
+  </Box>
+);
 
 export default function App() {
   return (
-    <ThemeProvider theme={{}}>
+    <ThemeProvider theme={appTheme}>
       <Box
         component='main'
         sx={{
           height: '100vh',
-          backgroundColor: '#000',
+          backgroundColor: (theme) => theme.palette.grey[900],
         }}
       >
         <Header />
         <Layout>
-          <h1>Olá mundo</h1>
+          <h1>Welcome to React Router!</h1>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='about' element={<About />} />
+          </Routes>
         </Layout>
       </Box>
     </ThemeProvider>
