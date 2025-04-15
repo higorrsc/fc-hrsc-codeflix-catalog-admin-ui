b86987e71d458af221a90bc7dbb74d18fa60168e
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-14 21:30:21 -0300

    chore: add DataGrid to list category page

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,9 +1,42 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../app/hooks';
+import { selectCategories } from './categorySlice';
 
-export const ListCategory = () => (
-  <Box>
-    <Typography variant='h3' component='h1'>
-      List Category Page
-    </Typography>
-  </Box>
-);
+export const ListCategory = () => {
+  const categories = useAppSelector(selectCategories);
+
+  const rows: GridRowsProp = categories.map((category) => ({
+    id: category.id,
+    name: category.name,
+    description: category.description,
+  }));
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 150 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'description', headerName: 'Description', width: 150 },
+  ];
+
+  return (
+    <Box maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
+      <Box display='flex' justifyContent='flex-end'>
+        <Button
+          variant='contained'
+          color='secondary'
+          component={Link}
+          to='/categories/create'
+          style={{ marginBottom: '1rem' }}
+        >
+          New Category
+        </Button>
+      </Box>
+      <DataGrid
+        pageSizeOptions={[2, 10, 20, 50, 100]}
+        rows={rows}
+        columns={columns}
+      />
+    </Box>
+  );
+};
