9a4ea2185ecdb84ae74be5f209679069b3fa3852
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-16 20:22:35 -0300

    refactor: categories table functions

--- src/features/categories/components/CategoryTable.tsx
+++ src/features/categories/components/CategoryTable.tsx
@@ -1,4 +1,14 @@
-import { GridFilterModel } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Box, Button, IconButton, Typography } from '@mui/material';
+import {
+  DataGrid,
+  GridColDef,
+  GridFilterModel,
+  GridRenderCellParams,
+  GridRowsProp,
+  GridToolbar,
+} from '@mui/x-data-grid';
+import { Link } from 'react-router-dom';
 import { Results } from '../../../types/Category';
 
 type Props = {
@@ -22,5 +32,95 @@ export function CategoryTable({
   handleOnPageChange,
   handleOnPageSizeChange,
 }: Props) {
-  return <div>CategoryTable</div>;
+  const componentProps = {
+    toolbar: {
+      showQuickFilter: true,
+    },
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      renderCell: renderNameCell,
+    },
+    {
+      field: 'isActive',
+      headerName: 'Active',
+      flex: 1,
+      type: 'boolean',
+      renderCell: renderIsActiveCell,
+    },
+    {
+      field: 'createAt',
+      headerName: 'Create At',
+      flex: 1,
+    },
+    {
+      field: 'id',
+      headerName: 'Actions',
+      type: 'string',
+      flex: 1,
+      renderCell: renderActionCell,
+    },
+  ];
+
+  const rows: GridRowsProp = data ? mapDataToGridRows(data) : [];
+
+  function renderNameCell(rowData: GridRenderCellParams) {
+    return (
+      <Link
+        style={{ textDecoration: 'none' }}
+        to={`/categories/edit/${rowData.id}`}
+      >
+        <Typography color='primary'>{rowData.value}</Typography>
+      </Link>
+    );
+  }
+
+  function renderIsActiveCell(rowData: GridRenderCellParams) {
+    return (
+      <Typography color={rowData.value ? 'primary' : 'secondary'}>
+        {rowData.value ? 'Active' : 'Inactive'}
+      </Typography>
+    );
+  }
+
+  function renderActionCell(rowData: GridRenderCellParams) {
+    return (
+      <IconButton color='secondary' onClick={() => handleDelete(rowData.value)}>
+        <DeleteIcon />
+      </IconButton>
+    );
+  }
+
+  function mapDataToGridRows(data: Results) {
+    const { data: categories } = data;
+    return categories.map((category) => ({
+      id: category.id,
+      name: category.name,
+      description: category.description,
+      isActive: category.is_active,
+      createAt: new Date(category.created_at).toLocaleDateString('pt-BR'),
+    }));
+  }
+
+  return (
+    
+      <Box sx={{ display: 'flex', height: 600 }}>
+        <DataGrid
+          columns={columns}
+          rows={rows}
+          // disableColumnFilter
+          // disableColumnSelector
+          // disableDensitySelector
+          // disableRowSelectionOnClick
+          // pageSizeOptions={[2, 10, 20, 50, 100]}
+          // slotProps={componentProps}
+          // slots={{ toolbar: GridToolbar }}
+          />
+      </Box>
+    
+  );
 }
