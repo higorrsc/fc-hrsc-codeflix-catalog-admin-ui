d4ac416d15a9a6bccc36ff0d1a163dd6eb9283b2
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-17 21:05:01 -0300

    chore: add edit cast member page

--- src/features/cast/EditCastMember.tsx
+++ src/features/cast/EditCastMember.tsx
@@ -1,3 +1,65 @@
+import { Box, Paper, Typography } from '@mui/material';
+import { useSnackbar } from 'notistack';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { CastMember } from '../../types/CastMember';
+import {
+  initialState,
+  useGetCastMemberByIdQuery,
+  useUpdateCastMemberMutation,
+} from './castMemberSlice';
+import { CastMemberForm } from './components/CastMemberForm';
+
 export const EditCastMember = () => {
-  return <div>EditCastMember</div>;
+  const id = useParams().id || '';
+  const { data: castMember, isFetching } = useGetCastMemberByIdQuery({ id });
+
+  const [castMemberState, setCastMemberState] =
+    useState<CastMember>(initialState);
+  const [updateCastMember, status] = useUpdateCastMemberMutation();
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCastMemberState({ ...castMemberState, [name]: value });
+  };
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    await updateCastMember(castMemberState);
+  }
+
+  useEffect(() => {
+    if (castMember) {
+      setCastMemberState(castMember.data);
+    }
+    if (status.isSuccess) {
+      enqueueSnackbar('CastMember updated successfully!', {
+        variant: 'success',
+      });
+    }
+    if (status.error) {
+      enqueueSnackbar('Error updating castMember!', { variant: 'error' });
+    }
+  }, [castMember, status, enqueueSnackbar]);
+
+  return (
+    <Box>
+      <Paper>
+        <Box p={2}>
+          <Box mb={2}>
+            <Typography variant='h4'>Edit Cast Member</Typography>
+          </Box>
+        </Box>
+        <CastMemberForm
+          castMember={castMemberState}
+          isDisabled={status.isLoading}
+          isLoading={isFetching}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+        />
+      </Paper>
+    </Box>
+  );
 };
