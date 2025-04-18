70794fde781c9f6e164ac637b75e097e00713626
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-17 21:04:40 -0300

    chore: add create cast member page

--- src/features/cast/CreateCastMember.tsx
+++ src/features/cast/CreateCastMember.tsx
@@ -1,3 +1,56 @@
+import { Box, Paper, Typography } from '@mui/material';
+import { useSnackbar } from 'notistack';
+import { useEffect, useState } from 'react';
+import { CastMember } from '../../types/CastMember';
+import { initialState, useCreateCastMemberMutation } from './castMemberSlice';
+import { CastMemberForm } from './components/CastMemberForm';
+
 export const CreateCastMember = () => {
-  return <div>CreateCastMember</div>;
+  const [createCastMember, status] = useCreateCastMemberMutation();
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [castMemberState, setCastMemberState] =
+    useState<CastMember>(initialState);
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCastMemberState({ ...castMemberState, [name]: value });
+  };
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    await createCastMember(castMemberState);
+  }
+
+  useEffect(() => {
+    if (status.isSuccess) {
+      enqueueSnackbar('CastMember created successfully!', {
+        variant: 'success',
+      });
+      setIsDisabled(true);
+    }
+    if (status.error) {
+      enqueueSnackbar('Error creating castMember!', { variant: 'error' });
+    }
+  }, [status, enqueueSnackbar]);
+
+  return (
+    <Box>
+      <Paper>
+        <Box p={2}>
+          <Box mb={2}>
+            <Typography variant='h4'>Create Cast Member</Typography>
+          </Box>
+        </Box>
+        <CastMemberForm
+          castMember={castMemberState}
+          isDisabled={isDisabled}
+          isLoading={false}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+        />
+      </Paper>
+    </Box>
+  );
 };
