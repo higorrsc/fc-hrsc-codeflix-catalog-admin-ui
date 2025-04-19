7c95818e64c25a880ae99722fe5de8ad7225a83c
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-18 22:02:15 -0300

    chore: add cast member table test

---
+++ src/features/cast/components/CastMemberTable.test.tsx
@@ -0,0 +1,75 @@
+import { GridFilterModel } from '@mui/x-data-grid';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { CastMemberTable } from './CastMemberTable';
+
+const Props = {
+  data: {
+    data: [
+      {
+        id: '1',
+        name: 'Teste',
+        type: 1,
+        deleted_at: '',
+        created_at: '2025-04-18T21:00:00.000000Z',
+        updated_at: '2025-04-18T21:00:00.000000Z',
+      },
+    ],
+    links: {
+      first: 'http://localhost:8000/api/cast_members?page=1',
+      next: '',
+      prev: '',
+      last: 'http://localhost:8000/api/cast_members?page=1',
+    },
+    meta: {
+      from: 1,
+      to: 1,
+      path: 'http://localhost:8000/api/cast_members',
+      total: 1,
+      per_page: 1,
+      current_page: 1,
+      last_page: 1,
+    },
+  },
+  page: 1,
+  perPage: 10,
+  rowsPerPage: [10, 25, 50],
+  isFetching: false,
+  handleDelete: (id: string) => {},
+  handleFilter: (filterModel: GridFilterModel) => {},
+  handleOnPageChange: (page: number) => {},
+  handleOnPageSizeChange: (perPage: number) => {},
+};
+
+describe('Test CastMemberTable', () => {
+  it('should render CastMemberTable correctly', () => {
+    const { asFragment } = render(<CastMemberTable {...Props} />, {
+      wrapper: BrowserRouter,
+    });
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render CastMemberTable with loading', () => {
+    const { asFragment } = render(
+      <CastMemberTable {...Props} isFetching={true} />,
+      {
+        wrapper: BrowserRouter,
+      }
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render CastMemberTable with empty data', () => {
+    const { asFragment } = render(
+      <CastMemberTable
+        {...Props}
+        data={{ data: [], links: {} as any, meta: {} as any }}
+      />,
+      {
+        wrapper: BrowserRouter,
+      }
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
