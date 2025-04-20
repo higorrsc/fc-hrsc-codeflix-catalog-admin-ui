403487cf1cc8903b4a413104683b935ab5ac2798
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-19 21:46:09 -0300

    chore: add category form test

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,15 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
-  },
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
+  moduleNameMapper: { '\\.(css|less|scss|sass)$': 'identity-obj-proxy' },
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  testMatch: ['**/*.test.(ts|tsx)'],
+  transform: { '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest' },
 };
 
 export default config;
---
+++ src/features/categories/components/CategoryForm.test.tsx
@@ -0,0 +1,58 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { CategoryForm } from './CategoryForm';
+
+const Props = {
+  category: {
+    id: '1',
+    name: 'Categoria Teste',
+    description: 'Descrição da categoria',
+    is_active: true,
+    deleted_at: null,
+    created_at: '2025-04-18T21:00:00.000000Z',
+    updated_at: '2025-04-18T21:00:00.000000Z',
+  },
+  isDisable: false,
+  isLoading: false,
+  handleSubmit: jest.fn(),
+  handleChange: jest.fn(),
+  handleToggle: jest.fn(),
+};
+
+describe('Test CategoryForm', () => {
+  it('should render CategoryForm correctly', () => {
+    const { asFragment } = render(<CategoryForm {...Props} />, {
+      wrapper: BrowserRouter,
+    });
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render CategoryForm with loading state correctly', () => {
+    const { asFragment } = render(<CategoryForm {...Props} isLoading />, {
+      wrapper: BrowserRouter,
+    });
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render CategoryForm with disabled state correctly', () => {
+    const { asFragment } = render(<CategoryForm {...Props} isDisabled />, {
+      wrapper: BrowserRouter,
+    });
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render CategoryForm with disabled state and loading state correctly', () => {
+    const { asFragment } = render(
+      <CategoryForm {...Props} isDisabled isLoading />,
+      {
+        wrapper: BrowserRouter,
+      }
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
