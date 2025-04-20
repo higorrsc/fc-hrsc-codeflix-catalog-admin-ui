d703502acf827e2bc51feaa09f16a83c6d6a41b4
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-20 13:17:26 -0300

    chore: add collect coverage to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   transform: { '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest' },
+  collectCoverage: true,
 };
 
 export default config;
