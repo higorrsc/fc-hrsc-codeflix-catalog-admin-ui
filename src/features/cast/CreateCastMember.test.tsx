f3babf4ad63680b46149e710bc83e452d8c97eb8
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-19 22:36:21 -0300

    chore: add create cast member page test

---
+++ src/features/cast/CreateCastMember.test.tsx
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom';
+import { renderWithProviders } from '../../utils/test-utils';
+import { CreateCastMember } from './CreateCastMember';
+
+describe('Test CreateCastMember page', () => {
+  it('should render correctly', () => {
+    const { asFragment } = renderWithProviders(<CreateCastMember />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
