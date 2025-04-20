9667b5ef08282cbc090678da95fe29d21d23541a
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-20 14:24:04 -0300

    chore: add mocks to use in the tests

---
+++ src/features/cast/mocks/index.ts
@@ -0,0 +1,35 @@
+export const castMemberResponse = {
+  data: [
+    {
+      id: 'f8d5cf53-f3d1-4a87-9c9d-8fd133c4238b',
+      name: 'Quigley',
+      type: 2,
+      deleted_at: null,
+      created_at: '2025-04-20T16:55:48+0000',
+      updated_at: '2025-04-20T16:55:48+0000',
+    },
+    {
+      id: '81da0e26-1e95-46d0-8a6b-78a3a11c9178',
+      name: 'Crooks',
+      type: 1,
+      deleted_at: null,
+      created_at: '2025-04-20T16:55:48+0000',
+      updated_at: '2025-04-20T16:55:48+0000',
+    },
+  ],
+  links: {
+    first: 'http://localhost:8000/api/cast_members?page=1',
+    last: 'http://localhost:8000/api/cast_members?page=1',
+    prev: null,
+    next: '',
+  },
+  meta: {
+    current_page: 1,
+    from: 1,
+    last_page: 1,
+    path: 'http://localhost:8000/api/cast_members',
+    per_page: 15,
+    to: 2,
+    total: 100,
+  },
+};
---
+++ src/features/categories/mocks/index.ts
@@ -0,0 +1,28 @@
+export const categoryResponse = {
+  data: [
+    {
+      id: '9e871d7b-1113-4523-a624-d7f9ad7c2d97',
+      name: 'Cornsilk',
+      description: 'Dolorem consequatur rem voluptatem facere.',
+      is_active: true,
+      deleted_at: null,
+      created_at: '2025-04-20T16:55:45+0000',
+      updated_at: '2025-04-20T16:55:45+0000',
+    },
+  ],
+  links: {
+    first: 'http://localhost:8000/api/categories?page=1',
+    last: 'http://localhost:8000/api/categories?page=1',
+    prev: null,
+    next: '',
+  },
+  meta: {
+    current_page: 1,
+    from: 1,
+    last_page: 1,
+    path: 'http://localhost:8000/api/categories',
+    per_page: 15,
+    to: 1,
+    total: 100,
+  },
+};
