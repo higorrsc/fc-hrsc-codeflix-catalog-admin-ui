a1929d9d4c47a202ccad8b99f1aea008e14be7bb
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-20 14:24:45 -0300

    chore: export base url and uses msw at tests to intercept requests

--- src/features/categories/ListCategory.test.tsx
+++ src/features/categories/ListCategory.test.tsx
@@ -1,9 +1,31 @@
-import { renderWithProviders } from '../../utils/test-utils';
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
+import { renderWithProviders, screen } from '../../utils/test-utils';
+import { baseUrl } from '../api/apiSlice';
 import { ListCategory } from './ListCategory';
+import { categoryResponse } from './mocks';
+
+export const handlers = [
+  http.get(`${baseUrl}/categories`, () => {
+    return HttpResponse.json(categoryResponse);
+  }),
+];
+
+const server = setupServer(...handlers);
 
 describe('Test ListCategory page', () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
   it('should render correctly', () => {
     const { asFragment } = renderWithProviders(<ListCategory />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render loading state', () => {
+    renderWithProviders(<ListCategory />);
+    const loading = screen.getByRole('progressbar');
+    expect(loading).toBeInTheDocument();
   });
 });
