086ab3453a3995e03a4d8d481bbb7bd1a59998df
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-21 19:32:18 -0300

    change: update test-utils to use Memory Router to render components with id

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -3,11 +3,9 @@ import { render } from '@testing-library/react';
 import React, { PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
 
-import { ThemeProvider } from '@mui/system';
 import { SnackbarProvider } from 'notistack';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
 import { setupStore, type AppStore, type RootState } from '../app/store';
-import { appTheme } from '../config/theme';
 // As a basic setup, import your same slice reducers
 
 // This type interface extends the default options for render from RTL, as well
@@ -15,6 +13,8 @@ import { appTheme } from '../config/theme';
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
   preloadedState?: Partial<RootState>;
   store?: AppStore;
+  initialEntries?: string[];
+  routePath?: string;
 }
 
 export function renderWithProviders(
@@ -22,27 +22,42 @@ export function renderWithProviders(
   extendedRenderOptions: ExtendedRenderOptions = {}
 ) {
   const {
+    preloadedState,
     // Automatically create a store instance if no store was passed in
-    store = setupStore(),
+    store = setupStore(preloadedState),
+    initialEntries,
+    routePath,
     ...renderOptions
   } = extendedRenderOptions;
 
   const Wrapper = ({ children }: PropsWithChildren) => (
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-          <ThemeProvider theme={appTheme}>
-            <SnackbarProvider maxSnack={3}>{children}</SnackbarProvider>
-          </ThemeProvider>
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>
+    <Provider store={store}>
+      <SnackbarProvider maxSnack={3}>{children}</SnackbarProvider>
+    </Provider>
   );
+
+  let finalUi = ui;
+  if (initialEntries) {
+    const elementInsideRouter = routePath ? (
+      <Routes>
+        <Route path={routePath} element={ui}></Route>
+      </Routes>
+    ) : (
+      ui
+    );
+    finalUi = (
+      <MemoryRouter initialEntries={initialEntries}>
+        {elementInsideRouter}
+      </MemoryRouter>
+    );
+  } else {
+    finalUi = <BrowserRouter>{ui}</BrowserRouter>;
+  }
 
   // Return an object with the store and all of RTL's query functions
   return {
     store,
-    ...render(ui, { wrapper: Wrapper, ...renderOptions }),
+    ...render(finalUi, { wrapper: Wrapper, ...renderOptions }),
   };
 }
 
