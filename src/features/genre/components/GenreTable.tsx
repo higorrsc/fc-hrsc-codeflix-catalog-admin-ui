89825fb2d7a48d56d0a9a1639197fb171a5d9034
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-23 20:14:55 -0300

    chore: add genre table component

---
+++ src/features/genre/components/GenreTable.tsx
@@ -0,0 +1,142 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Box, IconButton, Typography } from '@mui/material';
+import {
+  DataGrid,
+  GridColDef,
+  GridFilterModel,
+  GridPaginationModel,
+  GridRenderCellParams,
+  GridRowsProp,
+  GridToolbar,
+} from '@mui/x-data-grid';
+import { Link } from 'react-router-dom';
+import { Results } from '../../../types/Genre';
+
+type Props = {
+  data: Results | undefined;
+  page: number;
+  perPage: number;
+  rowsPerPage?: number[];
+  isFetching: boolean;
+  handleDelete: (id: string) => void;
+  handleFilter: (filterModel: GridFilterModel) => void;
+  handleOnPageChange: (page: number) => void;
+  handleOnPageSizeChange: (perPage: number) => void;
+};
+
+export function GenreTable({
+  data,
+  page,
+  perPage,
+  rowsPerPage,
+  isFetching,
+  handleDelete,
+  handleFilter,
+  handleOnPageChange,
+  handleOnPageSizeChange,
+}: Props) {
+  const componentProps = {
+    toolbar: {
+      showQuickFilter: true,
+    },
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      renderCell: renderNameCell,
+    },
+    {
+      field: 'isActive',
+      headerName: 'Active',
+      flex: 1,
+      type: 'boolean',
+      renderCell: renderIsActiveCell,
+    },
+    {
+      field: 'id',
+      headerName: 'Actions',
+      type: 'string',
+      flex: 1,
+      renderCell: renderActionCell,
+    },
+  ];
+
+  const rows: GridRowsProp = data ? mapDataToGridRows(data) : [];
+
+  const rowCount = data?.meta.total || 0;
+
+  function renderNameCell(rowData: GridRenderCellParams) {
+    return (
+      <Link
+        style={{ textDecoration: 'none' }}
+        to={`/genres/edit/${rowData.id}`}
+      >
+        <Typography color='primary'>{rowData.value}</Typography>
+      </Link>
+    );
+  }
+
+  function renderIsActiveCell(rowData: GridRenderCellParams) {
+    return (
+      <Typography color={rowData.value ? 'primary' : 'secondary'}>
+        {rowData.value ? 'Active' : 'Inactive'}
+      </Typography>
+    );
+  }
+
+  function renderActionCell(rowData: GridRenderCellParams) {
+    return (
+      <IconButton
+        color='secondary'
+        onClick={() => handleDelete(rowData.value)}
+        data-testid='delete-genre-button'
+      >
+        <DeleteIcon />
+      </IconButton>
+    );
+  }
+
+  function mapDataToGridRows(data: Results) {
+    const { data: genres } = data;
+    return genres.map((genre) => ({
+      id: genre.id,
+      name: genre.name,
+      isActive: genre.is_active,
+      categories: genre.categories,
+      // createAt: new Date(genre.created_at).toLocaleDateString('pt-BR'),
+    }));
+  }
+
+  const handlePaginationModelChange = (
+    newPaginationModel: GridPaginationModel
+  ) => {
+    handleOnPageChange(newPaginationModel.page);
+    handleOnPageSizeChange(newPaginationModel.pageSize);
+  };
+
+  return (
+    <Box sx={{ display: 'flex', height: 600 }}>
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        disableColumnFilter
+        disableColumnSelector
+        disableDensitySelector
+        disableRowSelectionOnClick
+        pageSizeOptions={rowsPerPage}
+        paginationModel={{ page, pageSize: perPage }}
+        paginationMode='server'
+        filterMode='server'
+        loading={isFetching}
+        rowCount={rowCount}
+        slotProps={componentProps}
+        slots={{ toolbar: GridToolbar }}
+        onFilterModelChange={handleFilter}
+        onPaginationModelChange={handlePaginationModelChange}
+      />
+    </Box>
+  );
+}
