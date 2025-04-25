6bab6ff4b1ead6d9d4f7679d2356ae9077538ecd
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-25 19:16:16 -0300

    chore: add video slice configuration

---
+++ src/features/video/videoSlice.ts
@@ -0,0 +1,129 @@
+import { Results as CastMemberResults } from '../../types/CastMember';
+import { Results as CategoriesResults } from '../../types/Category';
+import { Results as GenreResults } from '../../types/Genre';
+import { Result, Results, Video, VideoParams } from '../../types/Video';
+
+import { apiSlice } from '../api/apiSlice';
+
+export const initialState: Video = {
+  id: '',
+  title: '',
+  description: '',
+  year_launched: 0,
+  opened: false,
+  rating: '',
+  duration: 0,
+  deleted_at: '',
+  created_at: '',
+  updated_at: '',
+  genres: [],
+  categories: [],
+  cast_members: [],
+  thumb_file_url: '',
+  banner_file_url: '',
+  trailer_file_url: '',
+  video_file_url: '',
+};
+
+const endpointUrl = '/videos';
+
+function parseQueryParams(params: VideoParams) {
+  const queryParams = new URLSearchParams();
+
+  if (params.page) {
+    queryParams.append('page', params.page.toString());
+  }
+  if (params.perPage) {
+    queryParams.append('per_page', params.perPage.toString());
+  }
+  if (params.search) {
+    queryParams.append('search', params.search);
+  }
+
+  return queryParams.toString();
+}
+
+function getAllCategories() {
+  return `categories?all=true`;
+}
+
+function getAllGenres() {
+  return `genres?all=true`;
+}
+
+function getAllCastMembers() {
+  return `cast_members?all=true`;
+}
+
+// function createVideoMutation(data: VideoPayload) {
+//   return {
+//     url: endpointUrl,
+//     method: 'POST',
+//     body: data,
+//   };
+// }
+
+function getVideoById({ id }: { id: string }) {
+  return `${endpointUrl}/${id}`;
+}
+
+function getVideos({ page = 1, perPage = 10 }: VideoParams) {
+  const params = { page, perPage };
+  return `${endpointUrl}?${parseQueryParams(params)}`;
+}
+
+function deleteVideo({ id }: { id: string }) {
+  return {
+    url: `${endpointUrl}/${id}`,
+    method: 'DELETE',
+  };
+}
+
+// function updateVideo(data: VideoPayload) {
+//   return {
+//     url: `${endpointUrl}/${data.id}`,
+//     method: 'PUT',
+//     body: data,
+//   };
+// }
+
+export const videoApiSlice = apiSlice.injectEndpoints({
+  endpoints: ({ query, mutation }) => ({
+    getAllCategories: query<CategoriesResults, void>({
+      query: getAllCategories,
+    }),
+    getAllCastMembers: query<CastMemberResults, void>({
+      query: getAllCastMembers,
+    }),
+    getAllGenres: query<GenreResults, void>({
+      query: getAllGenres,
+    }),
+    // createVideo: mutation<Video, VideoPayload>({
+    //   query: createVideoMutation,
+    //   invalidatesTags: ['Videos'],
+    // }),
+    deleteVideo: mutation<Video, { id: string }>({
+      query: deleteVideo,
+      invalidatesTags: ['Videos'],
+    }),
+    getVideoById: query<Result, { id: string }>({
+      query: getVideoById,
+      providesTags: ['Videos'],
+    }),
+    getVideos: query<Results, VideoParams>({
+      query: getVideos,
+      providesTags: ['Videos'],
+    }),
+    // updateVideo: mutation<Video, VideoPayload>({
+    //   query: updateVideo,
+    //   invalidatesTags: ['Videos'],
+    // }),
+  }),
+});
+
+export const {
+  useDeleteVideoMutation,
+  useGetAllCategoriesQuery,
+  useGetVideoByIdQuery,
+  useGetVideosQuery,
+} = videoApiSlice;
