6480a9809b0505f56a2fc708396a5323ff3be9ee
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-25 21:31:09 -0300

    chore: implements video form component at create and edit video pages

--- src/features/video/CreateVideo.tsx
+++ src/features/video/CreateVideo.tsx
@@ -1,10 +1,17 @@
 import { Page } from '../../components/Page';
 import { VideoForm } from './components/VideoForm';
+import { initialState } from './videoSlice';
 
 export const CreateVideo = () => {
   return (
     <Page title='Create Video'>
-      <VideoForm />
+      <VideoForm
+        video={initialState}
+        isLoading={false}
+        isDisabled={true}
+        handleChange={() => {}}
+        handleSubmit={() => {}}
+      />
     </Page>
   );
 };
--- src/features/video/EditVideo.tsx
+++ src/features/video/EditVideo.tsx
@@ -1,3 +1,57 @@
+import { useSnackbar } from 'notistack';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Video } from '../../types/Video';
+
+import { Page } from '../../components/Page';
+import { VideoForm } from './components/VideoForm';
+import { initialState, useGetVideoByIdQuery } from './videoSlice';
+
 export const EditVideo = () => {
-  return <div>EditVideo</div>;
+  const id = useParams<{ id: string }>().id as string;
+  const { enqueueSnackbar } = useSnackbar();
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [videoState, setVideoState] = useState<Video>(initialState);
+  // const [updateVideo, status] = useUpdateVideoMutation();
+  // const { data: categories } = useGetAllCategoriesQuery();
+  const { data: video, isFetching } = useGetVideoByIdQuery({ id });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setVideoState({ ...videoState, [name]: value });
+  };
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    // await updateVideo(mapVideoToForm(videoState));
+  }
+
+  useEffect(() => {
+    if (video) {
+      setVideoState(video.data);
+    }
+  }, [video]);
+
+  // useEffect(() => {
+  //   if (status.isSuccess) {
+  //     enqueueSnackbar('Video updated successfully!', { variant: 'success' });
+  //     setIsDisabled(true);
+  //   }
+  //   if (status.error) {
+  //     enqueueSnackbar('Error updating video!', { variant: 'error' });
+  //   }
+  // }, [status, enqueueSnackbar]);
+
+  return (
+    <Page title='Edit Video'>
+      <VideoForm
+        video={videoState}
+        isDisabled={isDisabled}
+        isLoading={isFetching}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+    </Page>
+  );
 };
