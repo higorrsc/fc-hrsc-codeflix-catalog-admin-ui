1493522696b46500cca0da0fd99fa1d488468e07
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-26 20:35:27 -0300

    chore: remove index css

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { setupStore } from './app/store';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root')!;
