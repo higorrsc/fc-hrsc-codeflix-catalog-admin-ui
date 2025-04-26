00ced9babdc22218cffb80c1635aa98db2406e0d
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-26 17:39:22 -0300

    chore: add AutoCompleteField component

---
+++ src/components/AutoCompleteField.tsx
@@ -0,0 +1,69 @@
+import {
+  Autocomplete,
+  AutocompleteRenderInputParams,
+  TextField,
+} from '@mui/material';
+import { CastMember } from '../types/CastMember';
+import { Category } from '../types/Category';
+import { Genre } from '../types/Genre';
+
+type Props = {
+  name: string;
+  label: string;
+  value: (Category | Genre | CastMember)[];
+  options: (Category | Genre | CastMember)[];
+  isDisabled?: boolean;
+  isLoading?: boolean;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const AutoCompleteField = ({
+  name,
+  label,
+  value,
+  options,
+  isDisabled = false,
+  isLoading = false,
+  handleChange,
+}: Props) => {
+  const handleRenderOption = (
+    props: React.HTMLAttributes<HTMLLIElement>,
+    option: Category | Genre | CastMember
+  ) => (
+    <li {...props} key={option.id}>
+      {option.name}
+    </li>
+  );
+
+  const isIdEqual = (
+    option: CastMember | Category | Genre,
+    value: CastMember | Category | Genre
+  ): boolean => option.id === value.id;
+
+  const handleRenderInput = (params: AutocompleteRenderInputParams) => (
+    <TextField {...params} label={label} data-testid={`${name}-input`} />
+  );
+
+  const handleOnChange = (
+    e: React.ChangeEvent<{}>,
+    newValue: (CastMember | Category | Genre)[]
+  ) => {
+    handleChange({ target: { name, value: newValue } } as any);
+  };
+
+  return (
+    <Autocomplete
+      data-testid={`${name}-search`}
+      disabled={isDisabled || !options}
+      getOptionLabel={(option) => option.name}
+      isOptionEqualToValue={isIdEqual}
+      loading={isLoading}
+      multiple
+      onChange={handleOnChange}
+      options={options}
+      renderInput={handleRenderInput}
+      renderOption={handleRenderOption}
+      value={value}
+    />
+  );
+};
