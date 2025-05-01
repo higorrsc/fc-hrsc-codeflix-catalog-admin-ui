800d48b95af4cba6aa682f1720de63f33264b5b1
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-05-01 20:11:56 -0300

    chore: remove LinearWithValueLabel component and export LinearProgressWithLabel component

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,12 +1,12 @@
-import Box from '@mui/material/Box';
-import LinearProgress, {
+import {
+  Box,
+  LinearProgress,
   LinearProgressProps,
-} from '@mui/material/LinearProgress';
-import Typography from '@mui/material/Typography';
-import { useEffect, useState } from 'react';
+  Typography,
+} from '@mui/material';
 
-function LinearProgressWithLabel(
-  props: LinearProgressProps & { value: number }
+export function LinearProgressWithLabel(
+  props: LinearProgressProps & { value?: number }
 ) {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center' }}>
@@ -17,29 +17,8 @@ function LinearProgressWithLabel(
         <Typography
           variant='body2'
           sx={{ color: 'text.secondary' }}
-        >{`${Math.round(props.value)}%`}</Typography>
+        >{`${Math.round(props.value ?? 0)}%`}</Typography>
       </Box>
     </Box>
   );
 }
-
-export const LinearWithValueLabel = () => {
-  const [progress, setProgress] = useState(10);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((prevProgress) =>
-        prevProgress >= 100 ? 10 : prevProgress + 10
-      );
-    }, 800);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  return (
-    <Box sx={{ width: '100%' }}>
-      <LinearProgressWithLabel value={progress} />
-    </Box>
-  );
-};
