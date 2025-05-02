5f9a47532f3c832b22616bae960174432ab0c9af
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-05-02 18:28:37 -0300

    chore: create ProtectedRoute component and create dummy login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Box, Typography } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
+import { Login } from './components/Login';
+import { ProtectedRoute } from './components/ProtectedRoute';
 import { CreateCastMember } from './features/cast/CreateCastMember';
 import { EditCastMember } from './features/cast/EditCastMember';
 import { ListCastMember } from './features/cast/ListCastMember';
@@ -20,23 +22,117 @@ export default function App() {
     <Layout>
       <UploadList />
       <Routes>
-        <Route path='/' element={<ListCategory />} />
+        {/* Login */}
+        <Route path='/login' element={<Login />} />
+        {/* Home */}
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <ListCategory />
+            </ProtectedRoute>
+          }
+        />
         {/* Categories */}
-        <Route path='/categories' element={<ListCategory />} />
-        <Route path='/categories/create' element={<CreateCategory />} />
-        <Route path='/categories/edit/:id' element={<EditCategory />} />
+        <Route
+          path='/categories'
+          element={
+            <ProtectedRoute>
+              <ListCategory />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/categories/create'
+          element={
+            <ProtectedRoute>
+              <CreateCategory />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/categories/edit/:id'
+          element={
+            <ProtectedRoute>
+              <EditCategory />
+            </ProtectedRoute>
+          }
+        />
         {/* Cast Members */}
-        <Route path='/cast-members' element={<ListCastMember />} />
-        <Route path='/cast-members/create' element={<CreateCastMember />} />
-        <Route path='/cast-members/edit/:id' element={<EditCastMember />} />
+        <Route
+          path='/cast-members'
+          element={
+            <ProtectedRoute>
+              <ListCastMember />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/cast-members/create'
+          element={
+            <ProtectedRoute>
+              <CreateCastMember />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/cast-members/edit/:id'
+          element={
+            <ProtectedRoute>
+              <EditCastMember />
+            </ProtectedRoute>
+          }
+        />
         {/* Genres */}
-        <Route path='/genres' element={<ListGenre />} />
-        <Route path='/genres/create' element={<CreateGenre />} />
-        <Route path='/genres/edit/:id' element={<EditGenre />} />
+        <Route
+          path='/genres'
+          element={
+            <ProtectedRoute>
+              <ListGenre />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/genres/create'
+          element={
+            <ProtectedRoute>
+              <CreateGenre />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/genres/edit/:id'
+          element={
+            <ProtectedRoute>
+              <EditGenre />
+            </ProtectedRoute>
+          }
+        />
         {/* Video */}
-        <Route path='/videos' element={<ListVideo />} />
-        <Route path='/videos/create' element={<CreateVideo />} />
-        <Route path='/videos/edit/:id' element={<EditVideo />} />
+        <Route
+          path='/videos'
+          element={
+            <ProtectedRoute>
+              <ListVideo />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/videos/create'
+          element={
+            <ProtectedRoute>
+              <CreateVideo />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/videos/edit/:id'
+          element={
+            <ProtectedRoute>
+              <EditVideo />
+            </ProtectedRoute>
+          }
+        />
         {/* 404 */}
         <Route
           path='*'
